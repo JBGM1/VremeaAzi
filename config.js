11a877b1508395edd73dbb2f4f14a36ed286719b
mrsolid <gitumihai59@gmail.com>
2026-05-07 10:46:02 +0300

    Update GEMINI_API_KEY in config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 window.WEATHER_API_KEY = "c047bf3b89e68cde131ec21ba9c530b1";
-window.GEMINI_API_KEY   = "AIzaSyC9lbBZseBzyhmQRdp2fnSb-KMxj9Guph0";
+window.GEMINI_API_KEY   = "AIzaSyAmFMYfhylw0NaPSPivW_t-JM-KKltFnr8";
